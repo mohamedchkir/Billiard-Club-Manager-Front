3785291960d6332e75e207380c5e9755031c0f14
mohamed chkir <mohamedchkir2@gmail.com>
2024-03-15 10:21:11 +0000

    MAIN: Work in Login page

--- BCM-FRONT/src/app/app-routing.module.ts
+++ BCM-FRONT/src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './page/home/home.component';
 import { ClubComponent } from './page/club/club.component';
+import { LoginComponent } from './page/login/login.component';
+import { RegisterComponent } from './page/register/register.component';
 
 const routes: Routes = [
   { path: '' , component: HomeComponent },
   { path: 'home' , component: HomeComponent },
-  { path: 'club', component: ClubComponent }
+  { path: 'club', component: ClubComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent }
 ];
 
 @NgModule({
